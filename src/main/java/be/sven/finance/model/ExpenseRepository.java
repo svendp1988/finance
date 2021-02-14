57d0bf274e026401dcc257ad41aeee567cb4907d
Sven De Potter <sven.depotter@hotmail.com>
2021-02-14 07:54:39 +0100

    initial commit

---
+++ src/main/java/be/sven/finance/model/ExpenseRepository.java
@@ -0,0 +1,43 @@
+package be.sven.finance.model;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ExpenseRepository {
+    private final Map<String, Expense> expenses;
+
+    public ExpenseRepository() {
+        this.expenses = new HashMap<>();
+    }
+
+    public Expense addExpense(Expense expense) {
+        return expenses.put(expense.getId(), expense);
+    }
+
+    public Expense findById(String id) {
+        return expenses.get(id);
+    }
+
+    public Collection<Expense> findAll() {
+        return expenses.values();
+    }
+
+    public Expense removeExpense(String id) {
+        return expenses.remove(id);
+    }
+
+    public Expense updateExpense(String id, Expense expense) {
+        removeExpense(id);
+        return addExpense(expense);
+    }
+
+    public void addAll(List<Expense> expenses) {
+        expenses.forEach(expense -> this.expenses.put(expense.getId(), expense));
+    }
+}
+
---
+++ src/main/java/be/sven/finance/model/ExpenseType.java
@@ -0,0 +1,10 @@
+package be.sven.finance.model;
+
+public enum ExpenseType {
+    SHOPPING,
+    HOUSEHOLD,
+    RELAXATION,
+    DUBIOUS,
+    RECURRING
+}
+
